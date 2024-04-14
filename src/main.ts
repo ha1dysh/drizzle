d4e42f0410a6d1eb1e997ebbdf74edda6528708f
ha1dysh <olekshaid@gmail.com>
2024-04-14 15:50:33 +0300

    inserting data

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,24 @@ import { db } from "./drizzle/db";
 import { UserTable } from "./drizzle/schema";
 
 async function main() {
-	await db.delete(UserTable);
+	// await db.delete(UserTable);
+	const user = await db
+		.insert(UserTable)
+		.values({
+			name: "John",
+			age: 33,
+			email: "john.doe@qwe.com",
+		})
+		.returning({
+			id: UserTable.id,
+			userName: UserTable.name,
+		})
+		.onConflictDoUpdate({
+			target: UserTable.email,
+			set: { name: "Updated Name" },
+		});
+
+	console.log(user);
 }
 
 main();
